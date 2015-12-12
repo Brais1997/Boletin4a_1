c180cd124dbc0c9bd6e56fd35f889fecf869ebb4
Brais <Brais@Bicho>
2015-12-12 13:28:01 +0100

    Fin

---
+++ src/boletin/pkg4_1/Boletin4_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg4_1;
+
+/**
+ *
+ * @author Brais
+ */
+public class Boletin4_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        incremento obx= new incremento();
+        obx.calculos();
+    }
+    
+}
